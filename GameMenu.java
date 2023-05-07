1675249abcd118df0215481a3ed366c7820b849b
Aisha <128395400+Aishaa15@users.noreply.github.com>
2023-05-07 14:18:53 +0000

    Armeen's main menu

--- GameMenu.java
+++ GameMenu.java
@@ -1,157 +1,122 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.Graphics;
+
+
 public class GameMenu extends JFrame
 {
-    // declare GUI components
-     JButton playButton;
-     JButton instructionsButton;
-     JButton quitButton;
-     
-     //create the constructor
-     public GameMenu()
-     { 
-        setTitle("Illusion Lane");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new GridLayout(4, 4));
+    
+    public static void main(String[] args)
+    {
+        Themain();
+    }
         
-        // Create header label
-        JLabel headerLabel = new JLabel("Illusion Lane", JLabel.CENTER);
-        headerLabel.setFont(new Font("Creepster", Font.BOLD, 30));
-        add(headerLabel);
-        
-        //create tab name
-        setTitle("Game Introduction");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
-        //set backrounf colour to green 
-        getContentPane().setBackground(new Color(48, 64, 34));
-         
-        // Create buttons
-        playButton = new JButton("Play");
-        instructionsButton = new JButton("Instructions");
-        quitButton = new JButton("Quit");
-        
-        // Set up layout
-        
-        // Set button sizes
-       // Set button sizes
-        Dimension buttonSize = new Dimension(200, 50);
-        playButton.setPreferredSize(buttonSize);
-        instructionsButton.setPreferredSize(buttonSize);
-        quitButton.setPreferredSize(buttonSize);
-        add(playButton);
-        add(instructionsButton);
-        add(quitButton);
+    public static void Themain()
+    {
+        //create main frame
+        JFrame screen = new JFrame("Illusion Lane Menu");
+        screen.setSize(3000, 3000);
+    
+       
+        //set background 
+        Container cp = screen.getContentPane();
+        cp.setBackground(new Color(61,85,12));
 
-        
-        // Pack and center window
-        pack();
-        setSize(600, 600);
-        setLocationRelativeTo(null);
+        //add image 
+        ImageIcon image = new ImageIcon("IMAGE.jpg");
+        JLabel label_image = new JLabel(image);
+        label_image.setBounds(550,10,600,500);
+        screen.add(label_image);
 
-        setVisible(true);
-        
-        //add action listners to the buttons 
-        playButton.addActionListener(new ActionListener()
+
+        JButton Exit = new JButton("Exit");
+        Exit.setBounds(700, 700, 300, 50);
+        screen.add(Exit);
+        Exit.addActionListener(new ActionListener()
+        {
+            public void actionPerformed (ActionEvent e)
+            {
+                screen.dispose();
+            }
+        });
+
+
+        JButton play_game = new JButton("Play game");
+        play_game.setBounds(700, 500, 300, 50);
+        screen.add(play_game);
+        play_game.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
-            {   
-                if (e.getSource() == playButton) 
-                {   
-                    // Create game window
-                    JFrame gameWindow = new JFrame();
-                    gameWindow.setTitle("Game");
-                    gameWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // dispose window on close
-                    gameWindow.setExtendedState(JFrame.MAXIMIZED_BOTH); // set to full screen
-                    gameWindow.setVisible(true);
-                    setVisible(false);
-                                    
-                        // Add back button
-                    JButton backButton = new JButton("Back");
-                    backButton.addActionListener(new ActionListener() 
-                    {
-                        public void actionPerformed(ActionEvent e) 
-                        {
-                            gameWindow.dispose();
-                            setVisible(true);
-                        }
-                
+            {
+                screen.dispose();
+                play_screen();
             
-                    });
-                    
-                    gameWindow.add(backButton, BorderLayout.SOUTH);
-                    gameWindow.add(backButton, BorderLayout.SOUTH);
-                    gameWindow.setVisible(true);
-                    setVisible(false);
-                }
-                
-               
-            }   
+            }
         });
-        
-        
-        //add action listners to the buttons 
-        instructionsButton.addActionListener(new ActionListener()
+
+      
+        JButton instructions = new JButton("Instructions");
+        instructions.setBounds(700, 600, 300, 50);
+        screen.add(instructions);
+        instructions.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
-            {   
-                if (e.getSource() == instructionsButton) 
-                {   
-                    // Create instructions window
-                    JFrame instructionsWindow = new JFrame();
-                    instructionsWindow.setTitle("Instructions");
-                    instructionsWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // dispose window on close
-                    instructionsWindow.setExtendedState(JFrame.MAXIMIZED_BOTH); // set to full screen
-                    instructionsWindow.setVisible(true);
-                    setVisible(false);
-                        
-                    // Add instructions text and back button
-                    JTextArea instructionsText = new JTextArea("These are the instructions.");
-                    instructionsText.setEditable(false);
-                    JButton backButton = new JButton("Back");
-                    backButton.addActionListener(new ActionListener() 
-                    {
-                        public void actionPerformed(ActionEvent e) 
-                        {
-                            instructionsWindow.dispose();
-                            setVisible(true);
-                        }
-                
-            
-                    });
-                    
-                    // Set up layout
-                    JPanel panel = new JPanel();
-                    panel.setLayout(new BorderLayout());
-                    panel.add(instructionsText, BorderLayout.CENTER);
-                    panel.add(backButton, BorderLayout.SOUTH);
-                    instructionsWindow.add(panel);
-                    
-                    instructionsWindow.setVisible(true);
-                    setVisible(false);
-                    
-                }
-                
-               
-            }   
+            {
+                screen.dispose();
+                instruction_screen();
+            }
         });
+
+
+        screen.setLayout(null);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        screen.setResizable(false);
+        screen.setVisible(true);
         
-        
-        //add action listners to the buttons 
-        quitButton.addActionListener(new ActionListener()
+    }
+    
+    public static void instruction_screen()
+    {
+        JFrame instr = new JFrame("Instructions");
+        instr.setSize(3000,3000);
+
+        JButton goBack = new JButton("Go back");
+        goBack.setBounds(30,50,150,20);
+        instr.add(goBack);
+        goBack.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
-            {   
-                if (e.getSource() == quitButton) 
-                {   
-                    // Close all windows and exit program
-                    dispose();
-                    System.exit(0);
-                }
-                
-               
-            }   
+            {
+                instr.dispose();
+                Themain();
+            }
         });
-        }
+
+        JLabel instructions = new JLabel("OBJECTIVE OF GAME: use the keys on your keyboard to get your character to the end of the maze ");
+        instructions.setBounds(75, 100, 500, 50);
+        instructions.setFont(new Font ("Serif", Font.PLAIN, 50));
+        instr.add(instructions);
+
+        instr.setLayout(null);
+        instr.setResizable(false);
+        instr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        instr.setVisible(true);
+    }
+    
+    //window to game
+    public static void play_screen()
+    {
+        JFrame play = new JFrame("Game");
+        play.setSize(3000,3000);
+
+        play.setLayout(null);
+        play.setResizable(false);
+        play.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        play.setVisible(true);
+    }
 }
