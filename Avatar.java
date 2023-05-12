2e9ae57339a39630f8206ae36da4410c0eccde0c
Aisha <128395400+Aishaa15@users.noreply.github.com>
2023-05-12 00:23:42 +0000

    updated code

---
+++ Avatar.java
@@ -0,0 +1,73 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+
+
+
+public class Avatar {
+    Maze lab=new Maze();
+    int x=40;
+    int y=40;
+    int width=40;
+    int height=40;
+    int movement=40;
+        
+  
+    public void paint(Graphics graphic){
+        graphic.setColor(Color.pink);
+        graphic.fillRect(x, y, width, height);
+        graphic.setColor(Color.white);
+        graphic.drawRect(x, y, width, height);
+        
+      
+        graphic.setColor(Color.white);
+        graphic.fillOval(x+16,y+3,8,8);
+        graphic.setColor(Color.white);
+        
+        //Brazos y hombros
+        graphic.fillRect(x+9,y+12,4,14);
+        graphic.fillRect(x+11,y+12,4,4);
+        graphic.fillRect(x+28,y+12,4,14);
+        graphic.fillRect(x+26,y+12,4,4);
+        graphic.fillRect(x+14,y+12,13,14);
+       
+        //Piernas
+        graphic.fillRect(x+14,y+24,6,14);
+        graphic.fillRect(x+21,y+24,6,14);
+    }
+    
+  
+    
+    public void keyPressed(KeyEvent event){
+        int[][]maze=lab.getMaze();
+        
+        if(event.getKeyCode()==37){//Mover a la izquierda
+            if(maze[y/40][(x/40)-1] !=1){
+                x=x-movement;
+            }
+        }
+        if(event.getKeyCode()==39){//Mover a la derecha
+            if(maze[y/40][(x/40)+1]!=1){
+                x=x+movement;
+            }
+        }
+        if(event.getKeyCode()==40){//Mover abajo
+            if(maze[(y/40)+1][x/40]!=1){
+                y=y+movement;
+            }
+        }
+        if(event.getKeyCode()==38){//Mover arriba
+            if(maze[(y/40)-1][x/40]!=1){
+                y=y-movement;
+            }
+        }
+        
+       
+        if(x==840 && y==440){
+            Game.changeLevel();
+            x=40;
+            y=40;
+        }
+    }
+}   
--- Game.java
+++ Game.java
@@ -1,3 +1,91 @@
-public class Game {
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+/**
+ * The Game panel inherits attributes and methods from the main class (or base class) "JPanel". 
+ * However, since "Game" is a subclass, it can add its own fields and methods, 
+ * which is a fundamental pillar of Object-Oriented Programming.
+ */
+public class Game extends JPanel{
+    Maze maze=new Maze();
+    Avatar avatar=new Avatar();
+    public static int level=1;
     
+   
+        
+    public  Game(){
+        addKeyListener(new KeyListener(){
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+            
+            @Override
+            public void keyPressed(KeyEvent e) {
+                avatar.keyPressed(e);
+            }
+        
+             @Override
+            public void keyReleased(KeyEvent e) {
+            
+            }
+        
+    });
+        setFocusable(true);
+    }
+    
+  
+
+    public void paint(Graphics graphic){
+        maze.paint(graphic);
+        avatar.paint(graphic);
+    }
+    
+    public static int changeLevel(){
+        return level++;
+    }
+    
+  
+
+    public static int getLevel(){
+        return level;
+    }
+    
+   
+
+    public static void main(String[]args){
+           JOptionPane.showMessageDialog(null, "Years ago, a scientist created a new animal and called it 'ernudo'.\nBut a fiercy fire destroyed his lab.\nNowadays, a great explorer called 'Martino', has decided to get in the ruins\nof that old lab, to proof that animal exists indeed, but he danger lies on...");
+           JOptionPane.showMessageDialog(null, "Will he survive? Ready to help him get out the maze?");
+           JOptionPane.showMessageDialog(null, "Remember, get out the maze before the monsters catch you!");
+           JFrame myWindow=new JFrame("AVATAR RUN ∫_☻");
+           Game game =new Game();
+           myWindow.add(game);
+           myWindow.setSize(920,540);
+           myWindow.setLocation(300,200);
+           myWindow.setVisible(true);
+           myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+           
+       
+           
+           
+           while (true){
+               try {
+                   Thread.sleep(10);
+            } catch (InterruptedException ex) {
+                 Logger.getLogger(Game.class.getName()).log(Level.SEVERE, "you lose");
+            }
+            myWindow.repaint();   
+            
+                       
+            if(getLevel()>3){
+                JOptionPane.showMessageDialog(null, "you did it! see you soon.");
+                System.exit(0);
+                }
+            }
+    }
 }
